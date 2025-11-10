303a0e81244188eb2b695ede5504e9726a2c3441
Artur <arturguimaraes.sk8@gmail.com>
2025-11-09 21:26:33 -0300

    removing files

--- frontend/.next/dev/server/chunks/ssr/9e3a1_chatbot-with-rag_frontend__next-internal_server_app_page_actions_cba21f0f.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/projects/chatbot-with-rag/frontend/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=9e3a1_chatbot-with-rag_frontend__next-internal_server_app_page_actions_cba21f0f.js.map
--- frontend/.next/dev/static/chunks/projects_chatbot-with-rag_frontend_src_app_favicon_ico_mjs_7e5788f7._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/43703_next_dist_f92f6fd1._.js"
-],
-    source: "dynamic"
-});
